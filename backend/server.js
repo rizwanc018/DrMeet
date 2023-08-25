29a9f62a29faace12707f1705f30c2a7e170bdf3
rizwanc018 <22193871+rizwanc018@users.noreply.github.com>
2023-08-25 19:45:43 +0530

    socket ping timeout changed

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,7 @@ const PORT = process.env.PORT || 5000
 //////////////////////////////////////////////////////
 const server = http.createServer(app)
 const io = new Server(server, {
+    pingTimeout:60000,
     cors: {
         origin: "*",
         methods: ["GET", "POST"],
